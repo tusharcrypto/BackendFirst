0a0bbac5826ddb574d2722def0410f0ceba82c45
Tushar Wankhede <wankhedetushar18@gmail.com>
2023-11-24 16:28:45 +0530

    after database connection complete

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+
+const connectDB = async ()=>{
+  try {
+
+  const connectionInstance= await mongoose.connect(`${process.env.MOGODB_URL}/${DB_NAME}`)
+
+  // this help to print the connection of database with our mongo db
+
+    console.log(`Database Connect Sucessfully !! Db host is${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("Error occured !",error)
+   process.exit(1)
+  }
+}
+
+export default connectDB
+
+
---
+++ src/index.js
@@ -0,0 +1,46 @@
+
+// we required this to load as early as possible to load because we wan all required env varaible should available to every method
+
+// require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv';
+dotenv.config({path:'./env'});
+ import connectDB from "./db/index.js";       
+// dotenv.config({path:'./env'})
+
+connectDB()
+
+
+
+
+ //-----------------------------------------------------
+// stemp 1: import mongoose
+// stem 2: write connection function in which we should use try catch to handle error and async wait beacause database are in the other continent
+// we comment the moongse and db name beacse commit it and not using it here now
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants"
+//  step 4: as it we are connecting db in the here index file only we have follwing express code alse
+// import express from "express"
+// const app = express()
+
+//  it is imediate exexuting function know as IIFY
+// ( async()=>{
+//   try {
+
+//      step 3: write the connection code and it rquires two kind of parameter 1 is db connection url and database name which we declare in the conatant
+
+//    await mongoose.connect(`${process.env.MOGODB_URL}/${DB_NAME}`)
+
+//     app have lots of litner but we use this to check the error
+//    app.on("error ocurred",(error)=>{
+//     console.error("error",error)
+//     throw error
+//    })
+//     app.listen(process.env.PORT,()=>{
+//       console.log(`App is listning on the port ${process.env.PORT}`)
+//     })
+//   } catch (error) {
+//     console.error("Error occured:",error);
+//     throw error
+//   }
+
+// })()
